420feb55079f4d1ab0c5251eba7f3053c3d1b159
unknown <kumarbijendra@gmail.com>
2017-10-17 10:19:42 +0200

    First commit

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomerTest {
+    @Test
+    public void shouldPrintStatementForRentedMovies() {
+        Movie movie = new Movie("DDLG", Movie.REGULAR);
+        assertEquals(2, 2);
+    }
+
+}
