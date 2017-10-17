bf0d5efd148ddc5a275486185425da40d4fbf797
unknown <kumarbijendra@gmail.com>
2017-10-17 17:03:01 +0200

    Rename variable for more clarity

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -23,17 +23,17 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
+            Rental rental = (Rental) rentals.nextElement();
             //determine amounts for each line
-            double thisAmount = each.getCharge();
+            double thisAmount = rental.getCharge();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
                     &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+                    rental.getDaysRented() > 1) frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
+            result += "\t" + rental.getMovie().getTitle() + "\t" +
                     String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
