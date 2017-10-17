f695dc25deba6016a1fea1d42cef9399885997e8
unknown <kumarbijendra@gmail.com>
2017-10-17 17:33:16 +0200

    Extract Frequent Renter Point

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -25,11 +25,7 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental rental = (Rental) rentals.nextElement();
             // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    rental.getDaysRented() > 1) frequentRenterPoints++;
+            frequentRenterPoints += getFrequentRenterPoints(rental);
             //show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" +
                     String.valueOf(rental.getCharge()) + "\n";
@@ -42,6 +38,16 @@ public class Customer {
                 +
                 " frequent renter points";
         return result;
+    }
+
+    private int getFrequentRenterPoints(Rental rental) {
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                &&
+                rental.getDaysRented() > 1) frequentRenterPoints++;
+        return frequentRenterPoints;
     }
 
 }
