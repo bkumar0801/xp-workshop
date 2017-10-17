b5af16bfdf7b876020fff15a2413945d6e7e10ea
unknown <kumarbijendra@gmail.com>
2017-10-17 15:25:17 +0200

    Add more tests for customer statement

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -4,19 +4,86 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
     @Test
-    public void shouldPrintStatementForRentedMovies() {
+    public void shouldPrintStatementForRegularMovieRentedForTwoDays() {
         Movie terminator = new Movie("Terminator", Movie.REGULAR);
-        Rental rentalForTerminator = new Rental(terminator, 3);
-        Movie jungleBook = new Movie("Jungle Book", Movie.CHILDRENS);
-        Rental rentalForJungleBook = new Rental(jungleBook, 2);
+        Rental rentalForTerminator = new Rental(terminator, 2);
         Customer adam = new Customer("Adam");
         adam.addRental(rentalForTerminator);
-        adam.addRental(rentalForJungleBook);
         String expectedStatement = "Rental Record for Adam\n" +
-                "\tTerminator\t3.5\n" +
-                "\tJungle Book\t1.5\n" +
-                "Amount owed is 5.0\n" +
-                "You earned 2 frequent renter points";
+                "\tTerminator\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points";
         assertEquals(expectedStatement, adam.statement());
+    }
+    @Test
+    public void shouldPrintStatementForRegularMovieRentedForMoreThanTwoDays() {
+        Movie terminator = new Movie("Terminator", Movie.REGULAR);
+        Rental rentalForTerminator = new Rental(terminator, 4);
+        Customer adam = new Customer("Kevin");
+        adam.addRental(rentalForTerminator);
+        String expectedStatement = "Rental Record for Kevin\n" +
+                "\tTerminator\t5.0\n" +
+                "Amount owed is 5.0\n" +
+                "You earned 1 frequent renter points";
+        assertEquals(expectedStatement, adam.statement());
+    }
+    @Test
+    public void shouldPrintStatementForChildrensMovieRentedForTwoDays() {
+        Movie jungleBook = new Movie("Jungle Book", Movie.CHILDRENS);
+        Rental rentalForJungleBook = new Rental(jungleBook, 2);
+        Customer ryan = new Customer("Ryan");
+        ryan.addRental(rentalForJungleBook);
+        String expectedStatement = "Rental Record for Ryan\n" +
+                "\tJungle Book\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points";
+        assertEquals(expectedStatement, ryan.statement());
+    }
+    @Test
+    public void shouldPrintStatementForChildrensMovieRentedForMoreThanThreDays() {
+        Movie jungleBook = new Movie("Jungle Book", Movie.CHILDRENS);
+        Rental rentalForJungleBook = new Rental(jungleBook, 4);
+        Customer ryan = new Customer("Ryan");
+        ryan.addRental(rentalForJungleBook);
+        String expectedStatement = "Rental Record for Ryan\n" +
+                "\tJungle Book\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points";
+        assertEquals(expectedStatement, ryan.statement());
+    }
+
+    @Test
+    public void shouldPrintStatementForNewReleaseMovieRented() {
+        Movie newton = new Movie("Newton", Movie.NEW_RELEASE);
+        Rental rentalForNewton = new Rental(newton, 4);
+        Customer rahul = new Customer("Rahul");
+        rahul.addRental(rentalForNewton);
+        String expectedStatement = "Rental Record for Rahul\n" +
+                "\tNewton\t12.0\n" +
+                "Amount owed is 12.0\n" +
+                "You earned 2 frequent renter points";
+        assertEquals(expectedStatement, rahul.statement());
+    }
+
+    @Test
+    public void shouldPrintStatementForMoreThanOneMoviesRented() {
+        Movie newton = new Movie("Newton", Movie.NEW_RELEASE);
+        Rental rentalForNewton = new Rental(newton, 1);
+        Movie jungleBook = new Movie("Jungle Book", Movie.CHILDRENS);
+        Rental rentalForJungleBook = new Rental(jungleBook, 4);
+        Movie terminator = new Movie("Terminator", Movie.REGULAR);
+        Rental rentalForTerminator = new Rental(terminator, 4);
+        Customer rahul = new Customer("Rahul");
+        rahul.addRental(rentalForJungleBook);
+        rahul.addRental(rentalForNewton);
+        rahul.addRental(rentalForTerminator);
+        String expectedStatement = "Rental Record for Rahul\n" +
+                "\tJungle Book\t3.0\n" +
+                "\tNewton\t3.0\n" +
+                "\tTerminator\t5.0\n" +
+                "Amount owed is 11.0\n" +
+                "You earned 3 frequent renter points";
+        assertEquals(expectedStatement, rahul.statement());
+
     }
 }
