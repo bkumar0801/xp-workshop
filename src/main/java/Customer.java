0bb1da29b11c5360c4b48ad1d903112cb5d9e914
unknown <kumarbijendra@gmail.com>
2017-10-17 16:15:44 +0200

    Rename method and variables name

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             //determine amounts for each line
-            double thisAmount = getThisAmount(each);
+            double thisAmount = amountFor(each);
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -46,21 +46,21 @@ public class Customer {
         return result;
     }
 
-    private double getThisAmount(Rental each) {
+    private double amountFor(Rental rental) {
         double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+        switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
                 break;
         }
         return thisAmount;
