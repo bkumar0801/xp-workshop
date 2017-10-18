4a7e2b09a7f2f66d466e093ddfdb09c76edac1c4
unknown <kumarbijendra@gmail.com>
2017-10-18 14:11:15 +0200

    Move getFrequentRenterPoint to Movie

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -39,4 +39,11 @@ public class Movie {
         }
         return amount;
     }
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
